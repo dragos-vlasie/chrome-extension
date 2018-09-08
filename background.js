02f2f0535db7be01b205b3624c25c2373d6442e5
ghismovd <vlasiedragos1@gmail.com>
2018-09-08 15:54:48 +0100

    aded text on the badge

--- background.js
+++ background.js
@@ -1,5 +1,30 @@
+console.log("hey you clicked me ")
+
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
+
+let highlit  = document.querySelector('.highlight2');
+let highlit1  = document.querySelector('.highlight1');
+theUrl = 'https://demo0563230.mockable.io/bathroom';
+fetch(theUrl)
+.then(function(response) {
+  return response.json();
+})
+.then(function(myJson) {
+ chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
+  chrome.browserAction.setBadgeText({text:myJson.bathroom2});
+});
+
+
 function buttonClicked() {
-    consol.log("hey you clicked me ")
-}
+console.log("pizde")
+
+        
+        };
+        
+        window.setInterval(function(){
+            theUrl = 'https://demo0563230.mockable.io/bathroom';   
+fetch(theUrl);
+console.log("merge")
+          }, 5000);
+    
